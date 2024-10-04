dbcd8eae51925305c81818b9512a9aa51fc301a0
NTTLoc <nguyenloc.1349@gmail.com>
2024-10-04 08:43:40 +0700

    update latest

--- src/pages/Private/member/profile/MyProfile.jsx
+++ src/pages/Private/member/profile/MyProfile.jsx
@@ -4,53 +4,66 @@ import { useSelector } from "react-redux";
 
 const MyProfile = () => {
   const userInfo = useSelector((state) => state.users.data.result);
+  console.log(userInfo);
 
   return (
-    // <div className="flex justify-center items-center">
-    //   {/* <div className="flex flex-row gap-10 w-[70vw] border border-solid border-gray-200 shadow-lg p-5 rounded-3xl">
-    //     <img src={assets.KoiPool} className="w-[10%] rounded-[50%]" />
-
-    //     <div className="flex flex-col justify-between items-start gap-2">
-    //       <p className="text-xl font-bold text-center mt-2">{userInfo?.name}</p>
-
-    //       <p className="text-lg text-gray-400">Username</p>
-    //       <b>{userInfo?.username}</b>
-    //     </div>
-    //   </div> */}
-    //   <form className="bg-primary"> zxczxczxc</form>
-    // </div>
-
     <div className="flex flex-col justify-center items-center min-h-screen">
       <h2 className="text-2xl font-bold text-center mb-4">My Profile</h2>
-      <form className="bg-gradient-to-b from-cyan-500 to-gray-600 p-8 rounded-3xl shadow-lg w-[80vw] ">
+      <form className="p-8 rounded-3xl shadow-lg w-[80vw]">
         {/* User image */}
-        <div className="flex justify-center mb-4">
+        <div className="flex justify-start gap-6 mb-8">
           <img src={assets.KoiPool} className="w-20 h-20 rounded-full" />
+
+          <div className="mb-4">
+            <label className="block text-xl font-medium">Username</label>
+            <p className="text-2xl font-semibold">{userInfo?.username}</p>
+          </div>
         </div>
 
-        {/* User */}
-        <div className="mb-4">
-          <label className="block text-sm font-medium text-gray-300">
-            Name
-          </label>
-          <p className="text-lg font-semibold">{userInfo?.name}</p>
-        </div>
+        <div className="grid grid-cols-2">
+          <div className="mb-4">
+            <label className="block text-xl font-medium">Last name</label>
+            <p className="text-lg font-semibold">{userInfo?.name}</p>
+          </div>
 
-        {/* Username */}
-        <div className="mb-4">
-          <label className="block text-sm font-medium text-gray-300">
-            Username
-          </label>
-          <p className="text-lg font-semibold">{userInfo?.username}</p>
+          <div className="mb-4">
+            <label className="block text-xl font-medium">First Name</label>
+            <p className="text-lg font-semibold">{userInfo?.name}</p>
+          </div>
+
+          <div className="mb-4">
+            <label className="block text-xl font-medium">Gender</label>
+            <p className="text-lg font-semibold">{userInfo?.name}</p>
+          </div>
+
+          <div className="mb-4">
+            <label className="block text-xl font-medium">Date of Birth</label>
+            <p className="text-lg font-semibold">{userInfo?.name}</p>
+          </div>
+
+          <div className="mb-4">
+            <label className="block text-xl font-medium">Email</label>
+            <p className="text-lg font-semibold">{userInfo?.name}</p>
+          </div>
+
+          <div className="mb-4">
+            <label className="block text-xl font-medium ">Address</label>
+            <p className="text-lg font-semibold">{userInfo?.username}</p>
+          </div>
+
+          <div className="mb-4">
+            <label className="block text-xl font-medium ">Phone</label>
+            <p className="text-lg font-semibold">{userInfo?.username}</p>
+          </div>
         </div>
 
         {/* Nút submit */}
-        <div className="flex justify-center">
+        <div className="flex justify-start">
           <button
             type="submit"
             className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
           >
-            Chỉnh Sửa
+            Edit
           </button>
         </div>
       </form>
