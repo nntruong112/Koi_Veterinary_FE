0c7105a34a79f2c1569c4c498bfd91c76e5eadde
NTTLoc <nguyenloc.1349@gmail.com>
2024-10-13 20:53:03 +0700

    bỏ bảo mật firebase

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyASN-xv5ShqNhBxKBhIBdM7Jmy8V5bL-jE",
+  authDomain: "koihealthservicecenter.firebaseapp.com",
+  projectId: "koihealthservicecenter",
+  storageBucket: "koihealthservicecenter.appspot.com",
+  messagingSenderId: "606445354071",
+  appId: "1:606445354071:web:3d2584ec9e1a2f652ef712",
+  measurementId: "G-9Z433CJ15R",
 };
 
 // Initialize Firebase
